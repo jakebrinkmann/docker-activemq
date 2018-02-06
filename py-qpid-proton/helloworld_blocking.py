fc6e3fff7f8ba9ee78ce1a5952d731cd431aea54
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-02-06 17:17:30 -0600

    MAINT: send infinite messages

--- py-qpid-proton/helloworld_blocking.py
+++ py-qpid-proton/helloworld_blocking.py
@@ -4,17 +4,23 @@ from __future__ import print_function
 import os
 import time
 
+import proton
 from proton import Message
 from proton.utils import BlockingConnection
 from proton.handlers import IncomingMessageHandler
 
-time.sleep(2)
+while time.sleep(1) is None:
+    try:
+        conn = BlockingConnection(os.environ["AMQP_HOST"],
+                                  user=os.environ["AMQP_USER"],
+                                  password=os.environ["AMQP_PASS"])
+        receiver = conn.create_receiver("examples")
+        sender = conn.create_sender("examples")
+        sender.send(Message(body="Hello World!"));
+        msg = receiver.receive(timeout=30)
+        print(msg.body)
+        receiver.accept()
+        conn.close()
+    except proton.ConnectionException as e:
+        print(e)
 
-conn = BlockingConnection(os.environ["AMQP_HOST"], user=os.environ["AMQP_USER"], password=os.environ["AMQP_PASS"])
-receiver = conn.create_receiver("examples")
-sender = conn.create_sender("examples")
-sender.send(Message(body="Hello World!"));
-msg = receiver.receive(timeout=30)
-print(msg.body)
-receiver.accept()
-conn.close()
